4ab242fd4eaff69c5ac7e70711248a354d71562f
Billy Babicz <wjbabicz@gmail.com>
2013-06-04 13:09:29 -0500

    Removing old files.

--- ex1.rb
+++
@@ -1,9 +0,0 @@
-puts "Hello World!"
-puts "Hello again."
-puts "I like typing this."
-puts "This is fun."
-puts 'Yay! Printing.'
-puts "I'd much rather you 'not.'"
-puts 'I "said" do not touch this.'
-puts "This is another line."
-# This is a comment.
