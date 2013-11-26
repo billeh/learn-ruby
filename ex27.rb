d7d20cd0a161178032c43de37bf75013d3fe7d85
Billy Babicz <wjbabicz@gmail.com>
2013-11-26 16:38:18 -0600

    Exercise 27 study reference.

---
+++ ex27.rb
@@ -0,0 +1,48 @@
+# --- NOT ---
+# These are pretty self explanatory.
+puts "NOT false/true exercises."
+puts "--------------------"
+puts "not false: #{not false}" # true, dummy.
+puts "not true: #{not true}" # false, duh.
+puts "\n"
+
+# --- AND ---
+# Think of a spigot that has two handles, both need to be open for water to flow.
+puts "The 'AND' exercises."
+puts "--------------------"
+puts "true and false: #{true and false}" 
+puts "false and true: #{false and true}"
+puts "true and true: #{true and true}"
+puts "false and false: #{false and false}"
+puts "\n"
+
+# --- OR ---
+# Think of a faucet where either handle can be turned and water will flow.
+puts "The 'OR' exercises."
+puts "-------------------"
+puts "true or false: #{true or false}"
+puts "false or true: #{false or true}"
+puts "true or true: #{true or true}"
+puts "false or false: #{false or false}"
+puts "\n"
+
+# --- NOT AND ---
+# Like the previous AND exercise, not the NOT equivalent.
+puts "The 'NOT AND' exercises."
+puts "-------------------"
+puts "NOT true and false: #{not(true and false)}"
+puts "NOT false and true: #{not(false and true)}"
+puts "NOT true and true: #{not(true and true)}"
+puts "NOT false and false: #{not(false and false)}"
+puts "\n"
+
+# --- NOT OR ---
+# Like the previous OR exercise, now the NOT equivalent.
+puts "The 'NOT OR' exercises."
+puts "-------------------"
+puts "NOT true or false: #{not(true or false)}"
+puts "NOT false or true: #{not (false or true)}"
+puts "NOT true or true: #{not(true or true)}"
+puts "NOT false or false: #{not(false or false)}"
+puts "\n"
+
